acc1f785c3153abdd4f8789ec8c12c225a183de9
Dann4 <37845405+Dann4@users.noreply.github.com>
2021-10-14 16:59:06 -0500

    Create Bienvenido1.java

---
+++ Bienvenido1.java
@@ -0,0 +1,8 @@
+//Programa para imprimir texto
+public class Bienvenido1
+{
+  //el método main empieza la ejecución de la aplicación en Java
+  public static void main(String args[]){
+    System.out.println("Bienvenido a la programación en Java!");
+  }//fin del método main
+}//fin de la clase Bienvenido1
